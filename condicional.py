2a8acbfb0276a0606ba6d94476b25ad924730508
Hellen_Mesquita <147111229+HELLENMESQUITA@users.noreply.github.com>
2024-07-03 21:39:39 -0300

    Add files via upload

---
+++ condicional.py
@@ -0,0 +1,23 @@
+#print ('Olá, Bem Vindo(a)')
+#idade =16
+#variavel que recebe valor inteiro
+#if idade == 16:
+#uma condição     
+#print('Você é menor de idade')
+#a mensagem que vai aparecer
+
+#idade = int(input('Qual é a sua idade ?: '))#variavel que recebe valor inteiro
+
+#if idade < 16:
+#print ('Você é menor de idade')
+#else:
+#print ('Você é maior de idade')
+
+fetaria = int(input('Qual é a sua idade ?: '))
+ 
+if fetaria > 18:
+     print ('Você pode assistir filme de Terror:')
+elif fetaria >= 14:     
+  print ('Você pode assistir filme de Ação:')
+else          : 
+  print ('Você pode assistir filme Infantil:')
---
+++ lacosderepeticao.py
@@ -0,0 +1,15 @@
+#x = 0
+#while x < 5:
+#nome = input ('Qual é o seu nome? ')
+#x=x+1
+
+x = 0
+animais = []
+
+while x < 5:
+    animal = input ('Cite 5 animais com a letra do seu nome? ')
+    animais.append(animal)
+    x=x+1
+
+print(animais)
+
